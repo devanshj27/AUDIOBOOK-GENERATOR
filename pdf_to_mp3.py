75e1c822ccc4f75cfb084259f5ec6d3b8b6f82db
Devansh Jaiswal <jaiswaldevansh27@gmail.com>
2024-01-09 11:41:17 +0530

    Update pdf_to_mp3.py

--- pdf_to_mp3.py
+++ pdf_to_mp3.py
@@ -1,15 +1,21 @@
 import PyPDF2
 
-pdf_dir = "VIDEOS/E lockheart - We were liars.pdf"
-
+pdf_dir = "abc"
+# replace abc : paste the file directory of the document you wish to convert into an mp3 file
 text = ''
 
 with open(pdf_dir, 'rb') as pdf_file:
     pdf_reader_1 = PyPDF2.PdfReader(pdf_file)
 
-    for page_number in range(14,16):
+
+    # for page_number in range(14,16):         # un-comment this for loop, here you can specify the page numbers/ range of page numbers you wish to convert to your audio-file
+    #     page = pdf_reader_1.pages[page_number]    
+    #     text += page.extract_text()
+
+    for page_number in range(1,pdf_reader_1.numPages):         # use this for loop if you wish to convert the whole pdf into your audio file.
         page = pdf_reader_1.pages[page_number]
         text += page.extract_text()
+
 
 import pyttsx3
 
